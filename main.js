9cad5469f684e83e262a502903f4ba74f0a55b7f
Jaygopal-Lenka <73235238+Jaygopal-Lenka@users.noreply.github.com>
2021-05-05 18:26:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(800, 500);
+  canvas.position(560,100);
+}
+
+function draw() {
+background('#6C91C2');
+}
